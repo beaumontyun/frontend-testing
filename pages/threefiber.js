69e3ca6934183b3ebd1b293a5942b3d3e215d7f0
Beaumont Yun <b.yun@hotmail.com>
2022-06-07 18:31:37 +0800

    Saving WIP

--- pages/threefiber.js
+++ pages/threefiber.js
@@ -7,19 +7,22 @@ softShadows();
 
 const Threefiber = () => {
     return (
-        <div className="bg-white fixed w-[100vw] h-[100vh] m-0 p-0 box-border">
+        <div className="bg-white fixed w-[100vw] h-[100vh] m-0 p-0 box-border flex flex-row gap-3">
+            <div className='text-center w-[40%] self-center'>
+                <p className='w-[80%] mx-auto font-bold text-xl uppercase'>This is a box <br /> an ultra awesome box <br />Try moving it </p>
+            </div>
             <Canvas>
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
                 <group>
                     <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
                         <planeBufferGeometry attach='geometry' args={[100, 100]} />
-                        <meshBasicMaterial color='yellow' />
+                        <meshDistanceMaterial color='white' />
                     </mesh>
                 </group>
                 <group>
                     <Box args={[1, 1, 1]} />
-                    <meshBasicMaterial color='blue' />
+                    <meshNormalMaterial color='blue' />
                 </group>
 
 
