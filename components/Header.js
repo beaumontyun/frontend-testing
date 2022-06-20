eedd451943ce2d5467699269a45eac7625e78f0f
Beaumont Yun <b.yun@hotmail.com>
2022-06-20 12:41:28 +0800

    adjusting nav

--- components/Header.js
+++ components/Header.js
@@ -4,13 +4,13 @@ import Link from 'next/link'
 
 const useHasHydrated = () => {
     const [hasHydrated, setHasHydrated] = useState(false);
-  
+
     useEffect(() => {
-      setHasHydrated(true);
+        setHasHydrated(true);
     }, []);
-  
+
     return hasHydrated;
-  };
+};
 
 const Header = () => {
     const hasHydrated = useHasHydrated();
@@ -24,11 +24,13 @@ const Header = () => {
 
     return (
         <div className="w-full bg-black py-4">
-            <div className="flex justify-between mx-auto gap-20 px-10 text-white">
+            <div className="flex justify-between mx-auto gap-20 px-10 text-white text-xl">
                 <Link href='/'>Home</Link>
-                    <div>
-                        Total bears: { hasHydrated && count <= 0 ? "Ursuscene" : hasHydrated && count}
-                    </div>
+                <div>
+                    <Link href='/zustand'>
+                        <p className="cursor-pointer">Total bears: {hasHydrated && count <= 0 ? "Ursuscene" : hasHydrated && count}</p>
+                    </Link>
+                </div>
             </div>
         </div>
     )
